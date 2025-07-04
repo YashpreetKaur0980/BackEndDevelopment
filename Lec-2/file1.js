ba786d9e9a4931aa4fdd81437e8d6da8fc310898
YashpreetKaur0980 <yashkaur0980.com>
2025-07-04 15:17:03 +0530

    Your commit message

---
+++ Lec-2/file1.js
@@ -0,0 +1,11 @@
+let file3=require("./file3");
+console.log(file3.mul(5,3));
+function mul(a,b){
+    return a*b;
+}
+
+
+
+module.exports={
+    mul
+}
---
+++ Lec-2/file3.js
@@ -0,0 +1,8 @@
+function div(a,b){
+    return a/b;
+}
+function mul(a,b){
+    return a*b;
+}
+module.exports.mul=mul
+module.exports.div=div
